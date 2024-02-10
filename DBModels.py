44767619242f23ca1ac5eab09d0ce2767e7f9442
Sviatoslav <sviatoslavzonov@yandex.ru>
2024-02-10 03:28:27 +0300

    change README1

--- DBModels.py
+++ DBModels.py
@@ -3,30 +3,26 @@ from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
-
 class Publisher(Base):
     __tablename__ = "publisher"
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True)
-
     book = relationship("Book", back_populates="publisher")
 
     def __str__(self):
         return f'{self.id}: {self.name}'
-
 
 class Book(Base):
     __tablename__ = "book"
 
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=40), unique=True)
-    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"),
-                             nullable=False)
-
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
     publisher = relationship(Publisher, back_populates="book")
     stock2 = relationship("Stock", back_populates="book2")
-
+    def __str__(self):
+        return f'{self.id}: ({self.title}, {self.id_publisher}, {self.publisher}, {self.stock2})'
 
 class Shop(Base):
     __tablename__ = "shop"
@@ -35,7 +31,8 @@ class Shop(Base):
     name = sq.Column(sq.String(length=40), unique=True)
 
     stock = relationship("Stock", back_populates="shop")
-
+    def __str__(self):
+        return f'{self.id}: ({self.name}, {self.stock})'
 
 class Stock(Base):
     __tablename__ = "stock"
@@ -46,11 +43,11 @@ class Stock(Base):
     id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"),
                         nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
-
     shop = relationship(Shop, back_populates="stock")
     book2 = relationship(Book, back_populates="stock2")
     sale = relationship("Sale", back_populates="stock3")
-
+    def __str__(self):
+        return f'{self.id}: ({self.id_book}, {self.id_shop}, {self.count}, {self.shop}, {self.book2}, {self.sale})'
 
 class Sale(Base):
     __tablename__ = "sale"
@@ -61,9 +58,9 @@ class Sale(Base):
     id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"),
                          nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
-
     stock3 = relationship(Stock, back_populates="sale")
-
+    def __str__(self):
+        return f'{self.id}: ({self.price}, {self.date_sale}, {self.count}, {self.id_stock}, {self.stock3})'
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
--- main.py
+++ main.py
@@ -18,8 +18,12 @@ session = Session()
 
 create_tables(engine)
 
-with open('tests_data.json', 'r') as db:
+with open('test_data1.json', 'r') as db:
     data = json.load(db)
+
+#  Для 3 задания
+# with open('tests_data.json', 'r') as db:
+#     data = json.load(db)
 
 for line in data:
     method = {
@@ -32,17 +36,31 @@ for line in data:
     session.add(method(id=line['pk'], **line.get('fields')))
 
 session.commit()
+session.close()
 
-#доработать вывод
+#вывод по id
 
-request = input('Введи имя писателя или id для вывода: ')
-result = (session.query(Book, Shop, Sale).
-          filter(Publisher.name == request).
-          filter(Publisher.id == Book.id).
-          filter(Book.id_publisher == Stock.id_book).
-          filter(Stock.id_shop == Shop.id).
-          filter(Stock.id == Sale.id_stock).all())
-for r in result:
-    print(f'{r[0]} | {r[1]} | {r[2]}')
+# request = Publisher.id == input("Введите идентификатор издателя: ")
+# q = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).\
+#     join(Publisher).join(Stock).join(Sale).join(Shop).\
+#         filter(request).order_by(Sale.date_sale)
+# for book, shop, price, date in q:
+#         print(f'{book:<40} | {shop:<25} | {price:<10} | {date}')
+#
+# session.close()
 
+# вывод по имени и id
+request = input("Введите идентификатор или имя издателя: ")
+if request.isnumeric():
+    q = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).\
+        join(Publisher).join(Stock).join(Sale).join(Shop).\
+            filter(Publisher.id == int(request)).order_by(Sale.date_sale)
+    for book, shop, price, date in q:
+        print(f'{book:<40} | {shop:<25} | {price:<10} | {date}')
+else:
+    q = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale). \
+        join(Publisher).join(Stock).join(Sale).join(Shop). \
+        filter(Publisher.name.like(f'%{request}%')).order_by(Sale.date_sale)
+    for book, shop, price, date in q:
+        print(f'{book:<40} | {shop:<25} | {price:<10} | {date}')
 session.close()
